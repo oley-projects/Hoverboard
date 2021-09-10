4a9fe001733fbfd7f0874723442e0c0a3f9d28c7
oley-projects <oley.for.git@gmail.com>
2021-09-10 22:13:53 +0300

    hoverboard animation

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.getElementById('container');
+
+const SQUARES = 500;
+
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => setColor(square));
+
+    square.addEventListener('mouseout', () => removeColor(square));
+
+    container.appendChild(square);
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d';
+    element.style.boxShadow = '0 0 2px #000';
+}
+
+function getRandomColor() {
+    const letters = '0123456789abcdef';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
